dc0b910391bb693474414d2297c935b4060e22d4
qkrtkdhs <bagsaon@gmail.com>
2022-12-10 12:21:31 +0900

    .

---
+++ pygame/exam01.py
@@ -0,0 +1,7 @@
+import random
+
+ps = ["박찬", "케인", "정재성", "정재민" ]
+pc = ["코드플레이(용문)", "코드플레이(양평)", "ㅇㅇㅅㅋㄹ", "정상수네 집"]
+tl = ["테이저건", "느타리버섯", "타지리", "코딩쌤"]
+
+print(f"{random.choice(ps)}(이)가 {random.choice(pc)}에서 {random.choice(tl)}(으)로 사람 죽임")
---
+++ pygame/exam02.py
@@ -0,0 +1,7 @@
+def add(num):
+    result = 0
+    for number in range(num):
+        result += (number + 1)
+    return result
+
+print(add(4856))
